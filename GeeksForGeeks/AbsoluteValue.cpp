fd496a62a352f9934166e49ec52411de2a32c97d
gaurav19063 <gaurav19063@iiitd.ac.in>
2020-06-17 08:48:57 +0530

    Absolute no.

---
+++ GeeksForGeeks/AbsoluteValue.cpp
@@ -0,0 +1,10 @@
+
+// User function Template for C++
+
+// Should return absolute value of I
+// You need to complete this function
+int absolute(int I) {
+    // Your code here
+
+  return I>=0?I:I*(-1);
+}
