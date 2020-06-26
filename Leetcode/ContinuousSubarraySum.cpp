9cf1e205cdccc24297509d42cd533ec1397290f5
gaurav19063 <gaurav19063@iiitd.ac.in>
2020-06-27 00:24:07 +0530

    *

---
+++ Leetcode/ContinuousSubarraySum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+
+        for(int i=0;i<nums.size();i++)
+        { int sum=nums[i];
+
+            for(int j=i+1;j<nums.size();j++)
+            {
+                sum+=nums[j];
+                if(k==0&&sum==0)
+                    return true;
+               if(k!=0&&sum%k==0)
+
+               {
+                   return true;
+               }
+
+
+
+            }
+         // cout<<sum<<endl;
+        }
+        return false;
+    }
+};
